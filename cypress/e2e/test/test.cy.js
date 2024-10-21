1e9da70466d9a8d82201050575bd82ca626f188f
Truls Johan Jacobsen-Lamøy <truls@Trulss-MBP.home>
2024-10-21 15:35:05 +0200

    chore: refactored the code to be less repetetive

--- cypress/e2e/test/test.cy.js
+++ cypress/e2e/test/test.cy.js
@@ -1,39 +1,32 @@
-describe("Login test - valid credentials", () => {
-  it("Passes", () => {
-    cy.visit("http://127.0.0.1:5500/");
-    cy.get(".text-end .btn-outline-success").click();
-    cy.get("#loginEmail").type("truls@stud.noroff.no");
-    cy.get("#loginPassword").type("validPassword");
-    cy.get(".modal-footer .btn-success").contains("Login").click();
-  });
-});
+const baseUrl = "http://127.0.0.1:5500";
+const loginUser = (email, password) => {
+  cy.visit(baseUrl);
+  cy.get(".text-end .btn-outline-success").click();
+  cy.get("#loginEmail").type(email);
+  cy.get("#loginPassword").type(password);
+  cy.get(".modal-footer .btn-success").contains("Login").click();
+};
 
-describe("Logout Test", () => {
-  it("Passes", () => {
-    cy.visit("http://127.0.0.1:5500/");
-    cy.get(".text-end .btn-outline-success").click();
-    cy.get("#loginEmail").type("truls@stud.noroff.no");
-    cy.get("#loginPassword").type("validPassword");
-    cy.get(".modal-footer .btn-success").contains("Login").click();
-    cy.get(".text-end .btn-outline-warning").contains("Logout").click();
-    cy.window().then((window) => {
-      const localStorageLength = window.localStorage.length;
-      expect(localStorageLength).to.equal(0);
-    });
+describe("User Authentication Tests", () => {
+  it("Login with valid credentials", () => {
+    loginUser("truls@stud.noroff.no", "validPassword");
   });
-});
 
-describe("Login test - Invalid credentials", () => {
-  it("Fails", () => {
+  it("Login with invalid credentials", () => {
     cy.on("window:alert", (alertText) => {
       expect(alertText).to.equal(
         "Either your username was not found or your password is incorrect"
       );
     });
-    cy.visit("http://127.0.0.1:5500/");
-    cy.get(".text-end .btn-outline-success").click();
-    cy.get("#loginEmail").type("invalidEmail@stud.noroff.no");
-    cy.get("#loginPassword").type("invalidPassword");
-    cy.get(".modal-footer .btn-success").contains("Login").click();
+    loginUser("invalidEmail@stud.noroff.no", "invalidPassword");
+  });
+
+  it("Logout after login", () => {
+    loginUser("truls@stud.noroff.no", "validPassword");
+    cy.get(".text-end .btn-outline-warning").contains("Logout").click();
+    cy.window().then((window) => {
+      const localStorageLength = window.localStorage.length;
+      expect(localStorageLength).to.equal(0);
+    });
   });
 });
